7e78065df608d5cf1cd3d6d2e826e5f0fc75004b
Noel <ndalfaro333@gmail.com>
2023-08-23 00:55:32 -0500

    changing route file

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -17,7 +17,7 @@ const handler = NextAuth({
         email: session.user.email,
       });
 
-      session.user.is = sessionUser._id.toString();
+      session.user.id = sessionUser._id.toString();
       return session;
     },
     async signIn({ profile }) {
